c6ba91c79b88442c790f82d93a946e5c633026ab
AntoineSimoulin <antoine.simoulin@gmail.com>
2021-11-04 18:30:54 +0100

    :bug: fix import

--- pytree/__init__.py
+++ pytree/__init__.py
@@ -1,6 +1,2 @@
 from .data.data_collator import DataCollatorForTree
-from .parsers import PackedTree
-from .parsers.dependency_parser import DepGraph
-from .parsers.constituency_parser import ConstGraph
 from .models import ChildSumTree, ChildSumConfig, Similarity
-from .parsers.utils import GloveTokenizer
