9408799c4b8d4d59b1103e2205ffe7b250614f92
Antoine Simoulin <antoine.simoulin@gmail.com>
2021-11-15 11:58:24 +0100

    :wrench: change pypi package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pytree",
+    name="rvnn",
     version="0.0.1",
     author="Antoine Simoulin",
     author_email="antoine.simoulin@gmail.com",
